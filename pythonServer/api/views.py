393a123d72c9d4bfc62ffeef78a2aa8c98180f4a
Yubraj Bhandari <yubraj.bhandari.hero@gmail.com>
2022-04-30 11:48:29 +0545

    Django: Working in bot

--- pythonServer/api/views.py
+++ pythonServer/api/views.py
@@ -66,7 +66,7 @@ class WebHookView(View):
                                 {
                                     "title": "Is this the right picture?",
                                     "subtitle": "Tap a button to answer.",
-                                    "image_url": "https://veggiesserver.herokuapp.com/static/img/welcome.jpg",
+                                    "image_url": "veggiesserver.herokuapp.com/static/img/welcome.jpg",
                                     "buttons": [
                                         {
                                             "type": "postback",
